fbbf9b334a6161f3b065bc44590d84c1545f1a5e
alexgarcia <alexigarcia@msev.gob.mx>
2025-02-16 18:10:12 -0600

    Se agregaron los principales estilos scss y se creo el header

---
+++ src/app/layouts/header/header.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NavComponent } from "./nav/nav.component";
+
+@Component({
+  selector: 'app-header',
+  imports: [NavComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderComponent {
+
+}
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HeaderComponent } from "../../layouts/header/header.component";
+
+@Component({
+  selector: 'app-home',
+  imports: [HeaderComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomeComponent {
+
+}

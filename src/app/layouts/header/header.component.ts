09785abfdbbcd819cd6eb30f2704c8e4c01d3302
alexgarcia <alexigarcia@msev.gob.mx>
2025-02-24 19:51:50 -0600

    Se finalizo el componente separador de las lineas

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NavComponent } from "./nav/nav.component";
+import { SeparadorLineasComponent } from "../../shared/components/separador-lineas/separador-lineas.component";
 
 @Component({
   selector: 'app-header',
-  imports: [NavComponent],
+  imports: [NavComponent, SeparadorLineasComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from "../../layouts/header/header.component";
+import { SobreMiComponent } from "../../layouts/sobre-mi/sobre-mi.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SobreMiComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush

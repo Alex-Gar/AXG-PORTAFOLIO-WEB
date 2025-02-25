a0c28d0cff7c764aa1541786543344ee747e398d
alexgarcia <alexigarcia@msev.gob.mx>
2025-02-25 16:43:53 -0600

    Se finalizo la sección de las tecnologias para movil

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,10 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { NavComponent } from "./nav/nav.component";
-import { SeparadorLineasComponent } from "../../shared/components/separador-lineas/separador-lineas.component";
 
 @Component({
   selector: 'app-header',
-  imports: [NavComponent, SeparadorLineasComponent],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
---
+++ src/app/shared/components/separador-tecnologias/separador-tecnologias.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-separador-tecnologias',
+  imports: [],
+  templateUrl: './separador-tecnologias.component.html',
+  styleUrl: './separador-tecnologias.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SeparadorTecnologiasComponent {
+
+}
